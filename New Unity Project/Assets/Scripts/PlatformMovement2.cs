06a520cd6271dc10021ecbdf7ad7768f5fb1c711
LukeSunner <77940113+LukeSunner@users.noreply.github.com>
2021-04-19 04:09:15 +0100

    All vertical moving platforms added

---
+++ New Unity Project/Assets/Scripts/PlatformMovement2.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlatformMovement2 : MonoBehaviour
+{
+    private float MoveSpeed = 2f;
+    [SerializeField]private bool MoveUp = true;
+
+ 
+   
+    private void FixedUpdate()
+    {
+        if (transform.position.y > 10f)
+        {
+            MoveUp = false;
+        }
+        if (transform.position.y < 4f)
+        {
+            MoveUp = true;
+        }
+      
+        if (MoveUp)
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y + MoveSpeed * Time.deltaTime);
+        }
+        else
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y - MoveSpeed * Time.deltaTime);
+        }
+      
+    }
+}
